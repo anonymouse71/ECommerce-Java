2b07223fa24989ef21ebe1b8ecf40ddce74cd1b8
swetharavichandran03 <swetha.ravichandran@appdynamics.com>
2015-09-03 00:34:54 +0000

    Fix for 500 error

--- ECommerce-Web/src/main/java/com/appdynamicspilot/action/CartAction.java
+++ ECommerce-Web/src/main/java/com/appdynamicspilot/action/CartAction.java
@@ -16,42 +16,39 @@
 
 package com.appdynamicspilot.action;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.appdynamicspilot.jms.FulfillmentProducer;
-import com.appdynamicspilot.model.FulfillmentOrder;
-import com.opensymphony.xwork2.Action;
-import org.apache.log4j.Logger;
-import org.apache.struts2.interceptor.ServletRequestAware;
-import org.apache.struts2.interceptor.ServletResponseAware;
-
 import com.appdynamics.xml.CastorUtil;
-import com.opensymphony.xwork2.ActionContext;
-import com.opensymphony.xwork2.ActionSupport;
-import com.opensymphony.xwork2.Preparable;
-
 import com.appdynamicspilot.exception.OrderException;
 import com.appdynamicspilot.jms.CustomerMessageProducer;
+import com.appdynamicspilot.jms.FulfillmentProducer;
 import com.appdynamicspilot.jms.MessageProducer;
 import com.appdynamicspilot.model.Cart;
+import com.appdynamicspilot.model.FulfillmentOrder;
 import com.appdynamicspilot.model.Item;
 import com.appdynamicspilot.model.User;
-import com.appdynamicspilot.webserviceclient.*;
 import com.appdynamicspilot.service.CartService;
 import com.appdynamicspilot.service.ItemService;
 import com.appdynamicspilot.util.ArgumentUtils;
+import com.appdynamicspilot.webserviceclient.DotNetClient;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.Preparable;
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.tempuri.ArrayOfOrderDetail;
+import org.tempuri.OrderDetail;
 
-import org.tempuri.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CartAction extends ActionSupport implements Preparable,
         ServletResponseAware, ServletRequestAware {
     private static final Logger log = Logger.getLogger(CartAction.class);
+    /*List of items in the cart*/
+    List<Cart> cartsList;
     /*Cart persistence service to persist data to the database*/
     private CartService cartService;
     /*Item persistence service*/
@@ -63,11 +60,12 @@ public class CartAction extends ActionSupport implements Preparable,
     private CustomerMessageProducer customerMessageProducer;
     private HttpServletRequest request;
     private HttpServletResponse response;
-
-    /*List of items in the cart*/
-    List<Cart> cartsList;
-
     private boolean checkMe;
+
+    private static String generateInvoice(ArrayOfOrderDetail orderDetail) {
+        DotNetClient client = new DotNetClient();
+        return client.callService(orderDetail);
+    }
 
     public boolean isCheckMe() {
         return checkMe;
@@ -166,7 +164,7 @@ public class CartAction extends ActionSupport implements Preparable,
             }
         }
         List<Item> cartsList = cartService.getAllItemsByUser(user.getId());
-        log.info("the number of items in the cart are:" +cartsList.size());
+        log.info("the number of items in the cart are:" + cartsList.size());
         request.setAttribute("cartsList", cartsList);
         log.info("cartsList size" + cartsList.size());
         return "SUCCESS";
@@ -192,162 +190,124 @@ public class CartAction extends ActionSupport implements Preparable,
 
     /*Sending items from the cart to complete purchase called upon clicking the buy now button*/
     public String sendItems() {
-        log.info("sendItems check");
-        String fakeAmount = (String) ActionContext.getContext().get("orderAmount");
+        try {
+            String fakeAmount = (String) ActionContext.getContext().get("orderAmount");
+            User user = (User) ActionContext.getContext().getSession()
+                    .get("USER");
+            if (ArgumentUtils.isNull(user)) {
+                return "LOGOUT";
+            }
+            log.info("The current user is: " + user.getEmail());
+            Long userId = user.getId();
 
-        User user = (User) ActionContext.getContext().getSession()
-                .get("USER");
+            ArrayList<OrderDetail> orderDetailList = new ArrayList<OrderDetail>();
+            org.tempuri.ArrayOfOrderDetail arrayOfOrderDetail = new org.tempuri.ArrayOfOrderDetail();
 
-        log.info("The current user is: " +user.getEmail());
+            Cart cart = (Cart) ActionContext.getContext().getSession().get("CART");
+            if (cart == null) {
+                cart = new Cart();
+            }
+            cart = getCartService().getCartByUser(user.getId());
+            log.info("Cart belongs to:" + cart.getUser() + " The number of items associated with the user" + cart.getUser() + "is: " + cart.getItems());
 
-        if (ArgumentUtils.isNull(user)) {
-            return "LOGOUT";
-        }
+            List<Item> cartList = new ArrayList<>();
+            if (fakeAmount != null) {
+                cart.setAmount(Double.valueOf(fakeAmount));
+            }
+            if (cart != null) {
+                cartList = cartService.getAllItemsByUser(user.getId());
+                log.info("The items associated with the user are: " + cartList.size());
+                cart.getCartTotal();
+            } else {
+                cartList = Collections.EMPTY_LIST;
+            }
+            String orderIds = "";
+            String str1 = "";
+            String invoiceId = "";
+            String invoiceIds = "";
+            int outOfStock = 0;
 
-        Long userId = user.getId();
-        ArrayList<OrderDetail> orderDetailList = new ArrayList<OrderDetail>();
-        org.tempuri.ArrayOfOrderDetail arrayOfOrderDetail = new org.tempuri.ArrayOfOrderDetail();
-        Cart cart = (Cart) ActionContext.getContext().getSession()
-                .get("CART");
+            for (Item item : cartList) {
+                try {
+                    Long id = cartService.checkOut(item.getId(),
+                            cartService.getCartSize(userId));
 
-        log.info("Cart belongs to:" +cart.getUser() + " The number of items associated with the user" +cart.getUser() + "is: " +cart.getItems());
-
-        List<Item> cartList = null;
-
-        if (fakeAmount != null) {
-            cart.setAmount(Double.valueOf(fakeAmount));
-        }
-        if (cart != null) {
-            cartList = cartService.getAllItemsByUser(user.getId());
-            log.info("The items associated with the user are: " +cartList.size());
-            //trigger the mdic
-            cart.getCartTotal();
-        }
-        else {
-            cartList = Collections.EMPTY_LIST;
-        }
-        String orderIds = "";
-        String str1 = "";
-        String invoiceId = "";
-        String invoiceIds = "";
-        int outOfStock = 0;
-
-        for (Item item : cartList) {
-            try {
-                /**
-                 * Following code block is making fake call in order to generate
-                 * the exception and second method will make actual to inventory
-                 * server
-                 */
-                String error = (String) getServletRequest().getSession()
-                        .getAttribute("error");
-                log.debug("PARAM is: " + error);
-                if ("true".equalsIgnoreCase(error)) {
-                    try {
-                        callOrderService(cartService, item.getId(),
-                                "http://localhost:8080/services/OrderService?");
-                        getServletRequest().getSession().removeAttribute(
-                                "error");
-                    } catch (OrderException e) {
-                        log.error(
-                                "Something went wrong at inventory server ... retrying again!!!!!",
-                                e.getT().fillInStackTrace());
-                        log.error(e.getMessage());
-                        // log.debug("Something went wrong at inventory server ... retrying again!!!!!");
+                    OrderDetail orderDetail = new OrderDetail();
+                    orderDetail.setOrderId(id);
+                    orderDetail.setId(item.getId());
+                    orderDetail.setTitle(item.getTitle());
+                    arrayOfOrderDetail.getOrderDetail().add(orderDetail);
+                    orderIds = orderIds + id.toString() + ", ";
+                    if (id == 0) {
+                        outOfStock = 1;
                     }
+                    FulfillmentOrder order = new FulfillmentOrder(item, user);
+                    fulfillmentProducer.sendFulfillment(order);
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                    request.setAttribute("msg",
+                            "Error in creating order " + e.getMessage());
                 }
-                Long id = cartService.checkOut(item.getId(),
-                        cartService.getCartSize(userId));
+            }
 
-                OrderDetail orderDetail = new OrderDetail();
-                orderDetail.setOrderId(id);
-                orderDetail.setId(item.getId());
-                orderDetail.setTitle(item.getTitle());
-                /*
-                 * invoiceId = generateInvoice(orderDetail);
-				 * 
-				 * invoiceIds = invoiceIds+invoiceId.toString()+", ";
-				 */
+            if (checkMe) {
+                invoiceId = generateInvoice(arrayOfOrderDetail);
+            }
 
-                // orderDetailList.add(orderDetail);
-
-                arrayOfOrderDetail.getOrderDetail().add(orderDetail);
+            log.debug("ORDERS ARE " + orderIds);
+            if (!ArgumentUtils.isNullOrEmpty(orderIds) && outOfStock == 0) {
+                orderIds = orderIds.substring(0, orderIds.length() - 2);
+                log.debug("Request time(ms) in CartAction: sendItems"
+                        + System.currentTimeMillis());
+                messageProducer.sendMessageWithOrderId(orderIds, user.getEmail());
+                messageProducer.sendTextMessageWithOrderId();
+                customerMessageProducer.sendCustomerMesssage(user);
 
 
-                orderIds = orderIds + id.toString() + ", ";
-                if (id == 0) {
-                    outOfStock = 1;
+                if (invoiceId == "") {
+                    request.setAttribute("msg", "Order ID(s) for your order(s) : "
+                            + orderIds);
+                } else {
+                    request.setAttribute("msg",
+                            "Your Invoice ID for your order(s) " + orderIds + ": "
+                                    + invoiceId);
                 }
-                FulfillmentOrder order = new FulfillmentOrder(item, user);
-                fulfillmentProducer.sendFulfillment(order);
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+            } else {
                 request.setAttribute("msg",
-                        "Error in creating order " + e.getMessage());
+                        "Order not created as one or more items in your cart were out of stock");
             }
-
+            //deleting the cart instance associated with the user.
+            cartService.deleteCart(cart);
+            //creating an empty new cart instance and associating it with the user.
+            cart = new Cart();
+            cart.setUser(user);
+            ActionContext.getContext().getSession().put("CART", cart);
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
-
-        if (checkMe) {
-            invoiceId = generateInvoice(arrayOfOrderDetail);
-        }
-
-        log.debug("ORDERS ARE " + orderIds);
-        if (!ArgumentUtils.isNullOrEmpty(orderIds) && outOfStock == 0)
-        {
-            orderIds = orderIds.substring(0, orderIds.length() - 2);
-            log.debug("Request time(ms) in CartAction: sendItems"
-                    + System.currentTimeMillis());
-            messageProducer.sendMessageWithOrderId(orderIds, user.getEmail());
-            messageProducer.sendTextMessageWithOrderId();
-            customerMessageProducer.sendCustomerMesssage(user);
-
-
-            if (invoiceId == "")
-            {
-                request.setAttribute("msg", "Order ID(s) for your order(s) : "
-                        + orderIds);
-            }
-            else
-            {
-                request.setAttribute("msg",
-                        "Your Invoice ID for your order(s) " + orderIds + ": "
-                                + invoiceId);
-            }
-        }
-        else
-        {
-            request.setAttribute("msg",
-                    "Order not created as one or more items in your cart were out of stock");
-        }
-        //deleting the cart instance associated with the user.
-        cartService.deleteCart(cart);
-        //creating an empty new cart instance and associating it with the user.
-        cart = new Cart();
-        cart.setUser(user);
-        ActionContext.getContext().getSession().put("CART", cart);
         return "ENDPAGE";
     }
 
-    private static String generateInvoice(ArrayOfOrderDetail orderDetail) {
-        DotNetClient client = new DotNetClient();
-        return client.callService(orderDetail);
-    }
-
     public void prepare() throws Exception {
-    }
-
-    public void setCartService(CartService cartService) {
-        this.cartService = cartService;
     }
 
     public CartService getCartService() {
         return this.cartService;
     }
 
+    public void setCartService(CartService cartService) {
+        this.cartService = cartService;
+    }
+
     public MessageProducer getMessageProducer() {
         return this.messageProducer;
+    }
+
+    /**
+     * @param messageProducer The messageProducer to set.
+     */
+    public void setMessageProducer(MessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
     }
 
     public String getSelectedItemId() {
@@ -362,27 +322,20 @@ public class CartAction extends ActionSupport implements Preparable,
         this.itemService = itemService;
     }
 
-    /**
-     * @param messageProducer The messageProducer to set.
-     */
-    public void setMessageProducer(MessageProducer messageProducer) {
-        this.messageProducer = messageProducer;
+    public HttpServletRequest getServletRequest() {
+        return request;
     }
 
     public void setServletRequest(HttpServletRequest request) {
         this.request = request;
     }
 
-    public HttpServletRequest getServletRequest() {
-        return request;
+    public HttpServletResponse getServletResponse() {
+        return response;
     }
 
     public void setServletResponse(HttpServletResponse response) {
         this.response = response;
-    }
-
-    public HttpServletResponse getServletResponse() {
-        return response;
     }
 
     //Call OrderService Web Service (ECommerce-WS) to complete the order
